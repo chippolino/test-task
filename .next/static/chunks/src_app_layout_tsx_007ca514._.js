2fc593dd3c11665d328808d25be0d6970ced685f
User Unknown <userunknow@MacBookPro.local>
2025-07-11 01:09:26 +0300

    fix

--- .next/static/chunks/src_app_layout_tsx_007ca514._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_dist_73542e98._.js",
-  "static/chunks/src_a446c13a._.css"
-],
-    source: "dynamic"
-});
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: isProd ? '/test-task' : '',
+  assetPrefix: isProd ? '/test-task/' : '',
 };
 
 export default nextConfig;
